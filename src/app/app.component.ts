7969dc01f16028281e89738b3cd5aa8ad887e284
stigottarjensen <stig@nornir.io>
2024-01-30 21:15:51 +0100

    eweewr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,59 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   title = 'TriggMeFrontend';
-  rabatt_prosent:number =0.0;
-  average_purchase_count:number=0;
-  innkjopspris_prosent:number=0.0;
-  typisk_kjopesum:number=0.0;
-  triggme_avgift_prosent:number=0.0;
-  humaniter_andel_prosent:number=0.0;
-  total_purchase:number=0.0;
-  trigg_purchase:number=0.0;
-  tilgodelapp:number=0.0;
-  triggme_avgift:number=0.0;
-  humaniter_andel:number=0.0;
+  showImage = false;
+  qrCode = '';
+  triggmebody = {
+    discount_level: 0.0,
+    average_purchase_count: 0,
+    innkjopspris_prosent: 0.0,
+    average_purchase: 0.0,
+    triggme_fee_prosent: 0.0,
+    humaniter_fee_prosent: 0.0,
+    total_purchase: 0.0,
+    trigg_purchase: 0.0,
+    tilgodelapp: 0.0,
+    triggme_avgift: 0.0,
+    humaniter_andel: 0.0,
+  };
 
+  buckets: any[] = [];
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    this.http
-    .get('http://localhost:8778/AbisDB2JSON/abis/hive', {
+    let httpHeaders = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+    const httpOptions = {
+      headers: httpHeaders,
+      responseType: 'json',
+      observe: 'body',
       withCredentials: true,
-    })
-    .subscribe((result: any) => {});
+    };
+
+    this.http
+      // .get('http://localhost:8778/triggme/buysome', {
+      //     withCredentials: true
+      //   })
+      .post('http://localhost:8778/triggme/demo/setup', this.triggmebody, {
+        headers: httpHeaders,
+        responseType: 'json',
+        observe: 'body',
+        withCredentials: false,
+      })
+      .subscribe((result:any) => {
+       
+          this.buckets=result;
+        
+      });
   }
-
-
-
 }
